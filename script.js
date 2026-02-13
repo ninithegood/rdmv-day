24d601eac17320fadfc015a5fdeab1180eb422dd
ninithegood <59784981+ninithegood@users.noreply.github.com>
2026-02-13 16:51:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media.tenor.com/upgefKqHX0IAAAAd/yapapa.gif",    // 0 normal
+    "https://media.tenor.com/mPu5lB_ZyiUAAAAC/crunchy-cat-monkeycatluna.gif",  // 1 confused
+    "https://media.tenor.com/D3uchVyMt30AAAAC/marmotte-goatplaybanjo.gif",  // 2 pleading
+    "https://media.tenor.com/5N0m-99IRosAAAAi/cat-crying.gif",             // 3 sad
+    "https://media.tenor.com/gGP8ApJNu8kAAAAd/kenjou-cat.gif",       // 4 sadder
+    "https://media.tenor.com/NdyTDbwj2LIAAAAC/crying-dog.gif",             // 5 devastated
+    "https://media1.tenor.com/YYtWpKR5-qEAAAAd/fat-cat-vanished.gif",               // 6 very devastated
+    "https://media.tenor.com/31GZxaE5P5cAAAAC/funny-happy-happy-happy-cat.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
